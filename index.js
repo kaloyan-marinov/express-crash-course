9780ccc908b57a4f6227fe893173917cca087ce3
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-04 21:21:34 +0300

    create a `/api/members/:id` route + handle a GET request for it

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ const app = express();
 // });
 
 // Init middleware (function)
+/*
 app.use(logger);
+*/
 
 // Simple REST API
 app.get("/api/members", (req, res) => res.json(members));
+
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
 
 // Set static folder
 /*
