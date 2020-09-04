bc6de97dc7f42ff22c759b0a8d26e996d9297841
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-04 20:45:15 +0300

    create a web server that serves static HTML files

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  /*
-  res.send("<h1>Hello World!</h1>");
-  */
+// app.get("/", (req, res) => {
+//   /*
+//   res.send("<h1>Hello World!</h1>");
+//   */
 
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+//   /*
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+//   */
+// });
+
+// Set static folder
+/*
+can go to
+- localhost:5000/
+- localhost:5000/about.html
+*/
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
