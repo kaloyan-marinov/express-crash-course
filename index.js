3135ef0cae3e553d655bd1a8ddde12c51fb754ec
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-04 20:54:45 +0300

    create a `/api/members` route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 
+const members = require("./Members");
+
 const app = express();
 
 // app.get("/", (req, res) => {
@@ -20,6 +22,9 @@ can go to
 - localhost:5000/about.html
 */
 app.use(express.static(path.join(__dirname, "public")));
+
+// Simple REST API
+app.get("/api/members", (req, res) => res.json(members));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
