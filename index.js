a10d36a2d7898aa62f390aabc9409e242a0adcc8
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-04 20:31:31 +0300

    refine the call of `app.listen`

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
