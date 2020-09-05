7ef91203ae357d46de1c919e60720f8302807e56
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-05 09:49:44 +0300

    handle a POST request for the `/api/members/:id` route

--- routes/api/members.js
+++ routes/api/members.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const members = require("../../Members");
 
+// Get all members
 router.get("/", (req, res) => res.json(members));
 
+// Get single member
 router.get("/:id", (req, res) => {
   const found = members.some((member) => member.id === parseInt(req.params.id));
   if (found) {
@@ -12,6 +14,11 @@ router.get("/:id", (req, res) => {
   } else {
     res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
   }
+});
+
+// Create member
+router.post("/", (req, res) => {
+  res.send(req.body);
 });
 
 module.exports = router;
